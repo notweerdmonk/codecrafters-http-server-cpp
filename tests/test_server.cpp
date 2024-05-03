8bca143d3f314766b1a2015ac61a36711fc350d8
notweerdmonk <wrdmnk@gmail.com>
2024-05-03 08:52:06 +0530

    Diff files in test program.

--- tests/test_server.cpp
+++ tests/test_server.cpp
@@ -2,22 +2,40 @@
 #include <thread>
 #include <chrono>
 #include <mutex>
+#include <memory>
 #include <string>
 #include <cstring>
 #include <fcntl.h>
 #include <curl/curl.h>
 
-#define NUM_THREADS 5
-#define NUM_REQUESTS 50
+#define NUM_THREADS 8
+#define NUM_REQUESTS 64
+
+std::mutex mtx;
+
+int diff(std::string& f1, std::string& f2) {
+  std::string command = "diff " + f1 + " " + f2;
+  char result[8];
+
+  std::shared_ptr<FILE> diff_proc(popen(command.c_str(), "r"), pclose);
+  if (!diff_proc) {
+    std::cerr << "Error executing diff command: " << strerror(errno) << '\n';
+    return EXIT_FAILURE;
+  }
+
+  if (fgets(result, sizeof(result), diff_proc.get()) == nullptr) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
 
 size_t write_callback(void* contents, size_t size, size_t nmemb,
-          std::string* response) {
+                      std::string* response) {
   size_t total_size = size * nmemb;
   response->append((char*)contents, total_size);
   return total_size;
 }
-
-std::mutex cout_mutex;
 
 int main(int argc, char** argv) {
   std::string uploadfile{};
@@ -45,73 +63,90 @@ int main(int argc, char** argv) {
 
   for (int i = 0; i < NUM_THREADS; i++) {
     threads[i] = new std::thread(
-      [&uploadfile, &nrequest, buf, nread]() {
+    [&uploadfile, &nrequest, buf, nread]() {
 
-        int num_requests_per_thread = NUM_REQUESTS / NUM_THREADS;
-        for (int j = 0; j < num_requests_per_thread; j++) {
-          std::unique_lock<std::mutex> lock(cout_mutex);
-          std::cout << "nrequest: " << ++nrequest << " j: " << j << '\n';
-          lock.unlock();
+      int num_requests_per_thread = NUM_REQUESTS / NUM_THREADS;
+      for (int j = 0; j < num_requests_per_thread; j++) {
+        std::unique_lock<std::mutex> lock(mtx);
+        int nreqlocal = ++nrequest;
+        lock.unlock();
 
-          CURL* curl = curl_easy_init();
+        int diff_res;
+        CURL* curl = curl_easy_init();
 
-          std::string url{};
+        std::string url{};
 
-          switch (nrequest % 5) {
-            case 0:
-              url = "http://localhost:4221";
-              break;
+        switch (nreqlocal % 5) {
+          case 0:
+            url = "http://localhost:4221";
+            break;
 
-            case 1:
-              url = "http://localhost:4221/echo/mangoes/in/summer";
-              break;
+          case 1:
+            url = "http://localhost:4221/echo/mangoes/in/summer";
+            break;
 
-            case 2:
-              url = "http://localhost:4221/user-agent";
-              break;
+          case 2:
+            url = "http://localhost:4221/user-agent";
+            break;
 
-            case 3:
-              url = "http://localhost:4221/files/testfile";
-              break;
+          case 3:
+            url = "http://localhost:4221/files/testfile";
+            break;
 
-            case 4:
-              url = "http://localhost:4221/files/newfile";
-          }
-
-          struct curl_slist *headers = nullptr;
-          std::string response;
-
-          curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-          curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
-          curl_easy_setopt(curl, CURLOPT_USERAGENT, "laggybrowser/0.01");
-          curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
-          curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-
-          if (nrequest % 5 == 4) {
-            curl_slist_append(headers,
-                "Content-Type: application/octet-stream");
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buf);
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, nread);
-          }
-
-          CURLcode res = curl_easy_perform(curl);
-          //std::this_thread::sleep_for(std::chrono::milliseconds(200));
-          if (res != CURLE_OK) {
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-          } else {
-            std::lock_guard<std::mutex> lock(cout_mutex);
-            std::cout << "Request # " << nrequest << " URL: " << url <<
-                  " Response: " << response << std::endl;
-          }
-
-          if (nrequest % 5 == 4) {
-            curl_slist_free_all(headers);
-            headers = nullptr;
-          }
-          curl_easy_cleanup(curl);
+          case 4:
+            url = "http://localhost:4221/files/newfile";
         }
 
+        struct curl_slist *headers = nullptr;
+        std::string response;
+
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "laggybrowser/0.01");
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+
+        if (nreqlocal % 5 == 4) {
+          curl_slist_append(headers,
+                            "Content-Type: application/octet-stream");
+          curl_easy_setopt(curl, CURLOPT_POSTFIELDS, buf);
+          curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, nread);
+        }
+
+        CURLcode res = curl_easy_perform(curl);
+
+        if (nreqlocal % 5 == 4) {
+          curl_slist_free_all(headers);
+          headers = nullptr;
+
+          if (res == CURLE_OK) {
+            std::string newfile =
+              std::string{uploadfile, 0, uploadfile.rfind("/") + 1}
+              + "newfile";
+            diff_res = diff(uploadfile, newfile);
+          }
+        }
+
+        if (res != CURLE_OK) {
+          std::cerr << "curl_easy_perform() failed: " <<
+                    curl_easy_strerror(res) << std::endl;
+        } else {
+          std::lock_guard<std::mutex> lock(mtx);
+          std::cout << "Request # " << nreqlocal << " URL: " << url;
+          if (nreqlocal % 5 == 4) {
+            if (diff_res) {
+              std::cout << " Files don't match";
+            } else {
+              std::cout << " Files match";
+            }
+          }
+          std::cout << " Response: " << response << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
       }
+
+    }
     );
   }
 
